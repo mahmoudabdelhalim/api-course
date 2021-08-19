3e7f442fd054a59b241abe4912fdc5756fa08c34
sabreenMohammed107 <sabreenm312@gmail.com>
2021-08-19 19:13:53 +0200

    product

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -20,6 +20,10 @@ class ProductController extends BaseController
         return $this->sendResponse($categories, 'All categories Retrieved  Successfully');
     }
     //GET LATEST PRODUCT
+    public function latest(){
+        $products = Product::orderBy('id', 'desc')->take(3)->get();
+        return $this->sendResponse($products, 'Last 3 product Retrieved  Successfully');
+    }
 //GET SUB CATEGORY
 public function subCategories(){
     $subCategories = Category::whereNotNull('parent_category_id')->get();
