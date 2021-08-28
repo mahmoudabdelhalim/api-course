0156dbcea1f8230301d5f94e5734ba71d9ec2dc7
sabreenMohammed107 <sabreenm312@gmail.com>
2021-08-28 16:24:25 +0200

    bb

--- app/Models/Product_size.php
+++ app/Models/Product_size.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product_size extends Model
 {
-    //
+    protected $table = 'product_sizes';
+    protected $primaryKey = 'id';
     protected $fillable = [
         'product_id',
         'size_id',
-       
+
     ];
     public function size(){
         return $this->belongsTo('App\Models\Size','size_id');
