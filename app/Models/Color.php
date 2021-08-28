6eb8e2faacf8b0569ff6caa8032021a71c5c5442
sabreenMohammed107 <sabreenm312@gmail.com>
2021-08-28 16:33:48 +0200

    bb

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Color extends Model
 {
     //
+    protected $table = 'colors';
     protected $fillable = [
         'name','colorid'
     ];
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -47,7 +47,8 @@ class Product extends Model
 
     public function color()
     {
-    return $this->belongsToMany('App\Models\Color', 'Product_colors');
+    return $this->belongsToMany('App\Models\Color', 'Product_colors', 'product_id',
+   'color_id');
     }
     public function details()
     {
