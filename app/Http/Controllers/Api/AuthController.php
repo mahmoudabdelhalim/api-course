72823a6dde32f73e6851c6bb31ec43a7e68bc563
sabreenMohammed107 <sabreenm312@gmail.com>
2021-10-21 16:40:03 +0200

    gg

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -187,7 +187,7 @@ if($device) {
             // return $this->sendResponse($response, 'User has been send mail');
             $user=User::where('email',$request->email)->first();
 
-            Mail::to($request->email)->send(new ResetPassword($user->name, $user->token));
+            Mail::to($request->email)->send(new ResetPassword($user->name, $user->id));
 
             if(Mail::failures() != 0) {
                 return $this->sendResponse($user, 'User has been send mail');
